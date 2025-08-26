72721c19ad98cac993abe4938658904876a5b209
mohd shamim <siddique.mshamim@gmail.com>
2025-08-26 13:42:39 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let icon = document.querySelector(".nav-icon")
+let navToggle = document.querySelector(".nav-toggle")
+
+icon.addEventListener("click", () => {
+    navToggle.classList.toggle("active")
+})
+
